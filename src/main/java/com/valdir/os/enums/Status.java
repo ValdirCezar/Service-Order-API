a44c4a90e957bec268325b26842d7572856b5d2e
Valdir Cezar <valdircezar312@gmail.com>
2020-12-08 15:43:30 -0300

    Status enum created

---
+++ src/main/java/com/valdir/os/enums/Status.java
@@ -0,0 +1,30 @@
+package com.valdir.os.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public enum Status {
+
+    OPEN(1, "Open"),
+    IN_PROGRESS(2, "In Progress"),
+    CLOSED(3, "Closed");
+
+    private Integer code;
+    private String prefix;
+
+    public static Status toEnum(Integer code) {
+        if(code.equals(null))
+            return null;
+
+        for(Status x : Status.values()) {
+            if(code.equals(x.getCode()))
+                return x;
+        }
+
+        throw new IllegalArgumentException("Inválid Id: " + code);
+    }
+}
