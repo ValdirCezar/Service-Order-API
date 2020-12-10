8bba3e5572c21ec512c045502b0abc1e0e7b7706
Valdir Cezar <valdircezar312@gmail.com>
2020-12-10 12:50:49 -0300

    Service Order - findById method implemented

--- src/main/java/com/valdir/os/controllers/ServiceOrderController.java
+++ src/main/java/com/valdir/os/controllers/ServiceOrderController.java
@@ -1,9 +1,28 @@
 package com.valdir.os.controllers;
 
+import com.valdir.os.domain.ServiceOrder;
+import com.valdir.os.services.ServiceOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/so")
 public class ServiceOrderController {
+
+    @Autowired
+    private ServiceOrderService service;
+
+    /**
+     * This method will find a Service Order by id
+     * @return a Service Order or null
+     * */
+    @GetMapping(value = "/find={id}")
+    public ResponseEntity<ServiceOrder> findByID(@PathVariable Integer id) {
+        ServiceOrder obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
--- src/main/java/com/valdir/os/services/ServiceOrderService.java
+++ src/main/java/com/valdir/os/services/ServiceOrderService.java
@@ -1,7 +1,24 @@
 package com.valdir.os.services;
 
+import com.valdir.os.domain.ServiceOrder;
+import com.valdir.os.repositories.ServiceOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ServiceOrderService {
+
+    @Autowired
+    private ServiceOrderRepository repository;
+
+    /**
+     * This method will find a Service Order by id
+     * @return a Service Order or null
+     * */
+    public ServiceOrder findById(Integer id) {
+        Optional<ServiceOrder> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
 }
